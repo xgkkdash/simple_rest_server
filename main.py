f6e9fe1a59a88cd0796d5d85c22bdcc4eaaf6f64
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2020-12-19 03:30:24 +0800

    implement post_handler and get_handler

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse, abort
 import os
 import socket
 
+# create flask app
 app = Flask(__name__)
 api = Api(app)
 
+# define parser for POST
+parser = reqparse.RequestParser()
+parser.add_argument('key')
+parser.add_argument('value')
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+# dict for record key-value pair
+kv_pair = {}
 
 
-api.add_resource(HelloWorld, '/')
+class GetHandler(Resource):
+    def get(self, key):
+        # return value if key is in kv_pair, else return 404 Not Found
+        if key in kv_pair:
+            return kv_pair[key], 200
+        else:
+            return abort(404, message="Key {} doesn't exist".format(key))
+            # return 'Key Not Exist', 404
+
+
+class PostHandler(Resource):
+    def post(self):
+        # add kv-pair to dict to args are valid, else return 400 Bad Request
+        args = parser.parse_args()
+        key = args['key']
+        value = args['value']
+        if not key or not value:
+            return abort(400, message="POST request must contains both key and value")
+        kv_pair[key] = value
+        return {key: value}, 200
+
+
+api.add_resource(PostHandler, '/')
+api.add_resource(GetHandler, '/<string:key>')
 
 if __name__ == '__main__':
     host_name = socket.gethostname()
     host_ip = socket.gethostbyname(host_name)
     print("Hostname :  ", host_name)
     print("IP : ", host_ip)
-    # hostName = host_name or "localhost"
     serverPort = int(os.environ.get('PORT', 5000))
 
     app.run(host='0.0.0.0', port=serverPort, debug=True)

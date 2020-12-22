6afd7b39d8acfe71753331dbf3367ea82e5b08ad
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2020-12-23 04:18:48 +0800

    use with phrase to keep concurrency of db object in server

--- database/db.py
+++ database/db.py
@@ -1,12 +1,20 @@
 import sqlite3
 
+from flask import g
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('mydatabase.db', detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
 
 def init_db():  # return a db with default table created
-    db = sqlite3.connect('mydatabase.db')
+    db = get_db()
     cursor = db.cursor()
     cursor.execute("DROP TABLE IF EXISTS kvpairs")
     cursor.execute("CREATE TABLE kvpairs(key text PRIMARY KEY, value text)")
     db.commit()
-
-    return db
 
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from flask_restful import Resource, Api, reqparse, abort
 import os
 import socket
 
-from database.db import init_db
+from database.db import init_db, get_db
 
 # create flask app
 app = Flask(__name__)
 api = Api(app)
 
 # init db
-db = init_db()
+with app.app_context():
+    init_db()
 
 # define parser for POST/PUT
 parser = reqparse.RequestParser()
@@ -24,23 +25,25 @@ parser.add_argument('value')
 class GetDelHandler(Resource):
     def get(self, key):
         # if element exists in DB, return it with 200, else return 404 Not Found
-        cursor = db.cursor()
-        sql_str = "select * from kvpairs where key = '" + key + "'"
-        cursor.execute(sql_str)
-        rows = cursor.fetchall()
-        if rows:
-            element = rows[0]  # using primary key to search, should only get one result
-            return element, 200
-        else:
-            return abort(404, message="element with Key {} doesn't exist".format(key))
-            # return 'Key Not Exist', 404
+        with get_db() as db:
+            cursor = db.cursor()
+            sql_str = "select * from kvpairs where key = '" + key + "'"
+            cursor.execute(sql_str)
+            rows = cursor.fetchall()
+            if rows:
+                result_k, result_v = rows[0]  # using primary key to search, should only get one result
+                return {'key': result_k, 'value': result_v}, 200
+            else:
+                return abort(404, message="element with Key {} doesn't exist".format(key))
+                # return 'Key Not Exist', 404
 
     def delete(self, key):
-        cursor = db.cursor()
-        sql_str = "delete from kvpairs where key = '" + key + "'"
-        cursor.execute(sql_str)
-        db.commit()
-        return "Delete Done", 200
+        with get_db() as db:
+            cursor = db.cursor()
+            sql_str = "delete from kvpairs where key = '" + key + "'"
+            cursor.execute(sql_str)
+            db.commit()
+            return "Delete Done", 200
 
 
 class PostPutHandler(Resource):
@@ -52,12 +55,12 @@ class PostPutHandler(Resource):
 
         if self._key_exist(key):
             # can do DB update, return 200 after db update done
-            cursor = db.cursor()
-            sql_str = "UPDATE kvpairs SET value = '" + value + "' where key = '" + key + "'"
-            # 'UPDATE employees SET name = "Rogers" where id = 2'
-            cursor.execute(sql_str)
-            db.commit()
-            return "Update {} Success".format((key, value)), 200
+            with get_db() as db:
+                cursor = db.cursor()
+                sql_str = "UPDATE kvpairs SET value = '" + value + "' where key = '" + key + "'"
+                cursor.execute(sql_str)
+                db.commit()
+                return "Update {} Success".format((key, value)), 200
         else:
             return abort(400, message="primary key does not exist, cannot update")
 
@@ -70,11 +73,12 @@ class PostPutHandler(Resource):
             return abort(400, message="primary key already exist, cannot insert")
         else:
             # can do DB insert, return 201 created after db insert done
-            cursor = db.cursor()
-            sql_str = "INSERT INTO kvpairs(key, value) VALUES('" + key + "', '" + value + "')"
-            cursor.execute(sql_str)
-            db.commit()
-            return "Insert {} Success".format((key, value)), 201
+            with get_db() as db:
+                cursor = db.cursor()
+                sql_str = "INSERT INTO kvpairs(key, value) VALUES('" + key + "', '" + value + "')"
+                cursor.execute(sql_str)
+                db.commit()
+                return "Insert {} Success".format((key, value)), 201
 
     def _params_from_request(self):
         args = parser.parse_args()
@@ -83,11 +87,12 @@ class PostPutHandler(Resource):
         return key, value
 
     def _key_exist(self, key):
-        cursor = db.cursor()
-        sql_str = "select * from kvpairs where key = '" + key + "'"
-        cursor.execute(sql_str)
-        rows = cursor.fetchall()
-        return True if rows else False
+        with get_db() as db:
+            cursor = db.cursor()
+            sql_str = "select * from kvpairs where key = '" + key + "'"
+            cursor.execute(sql_str)
+            rows = cursor.fetchall()
+            return True if rows else False
 
 
 # add handler to api

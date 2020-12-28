994c752395be5f53a1ee05ddd9af7a639a4df5cf
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2020-12-29 02:20:11 +0800

    implement simple server using token authentication

--- auth/auth_client.py
+++ auth/auth_client.py
@@ -3,13 +3,17 @@ import requests
 base_url = 'http://0.0.0.0:5000'
 
 
-def test_unauthorized():
-    r = requests.get(base_url)
-    # Unauthorized Access, 401
-    assert r.status_code == 401
-
-
-def test_authenticated():
-    r = requests.get(base_url, auth=("root", "root"))
-    # authenticate success, 200
+def test_using_token_for_auth():
+    login_url = base_url + '/login'
+    r = requests.post(login_url, auth=("root", "root"))
     assert r.status_code == 200
+    token = r.content.decode("utf-8")
+
+    access_url = base_url + '/protected'
+    headers = {"Authorization": token}
+    r = requests.get(access_url, headers=headers)
+    assert r.status_code == 200
+
+
+if __name__ == '__main__':
+    test_using_token_for_auth()
--- auth/auth_server.py
+++ auth/auth_server.py
@@ -1,26 +1,68 @@
-from flask import Flask
-from flask_httpauth import HTTPBasicAuth
+import jwt
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
 
-users = {
-   "root": "root"
-}
+# set secret key for generate token
+app.config['SECRET_KEY'] = 'xgtest'
+
+users = {"root": "root"}
+
+# user login and get a token
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.authorization:
+        return jsonify({"message": "Post request must have auth"}), 401
+
+    username = request.authorization.get('username', None)
+    password = request.authorization.get('password', None)
+    if not username:
+        return jsonify({"message": "Missing username"}), 400
+    if not password:
+        return jsonify({"message": "Missing password"}), 400
+
+    if username not in users or password != users.get(username, None):
+        return jsonify({"message": "Wrong username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    access_token = generate_token_by_user_id(username)
+    return access_token, 200
 
 
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and password == users.get(username):
-        return username
+def generate_token_by_user_id(user_id):
+    try:
+        payload = {
+            'exp': datetime.utcnow() + timedelta(minutes=5),
+            'iat': datetime.utcnow(),
+            'user': user_id
+        }
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
+# user use token to access
+@app.route('/protected', methods=['GET'])
+def access_by_token():
+    token = request.headers.get('Authorization', None)
+    if not token:
+        return jsonify({"message": "token required"}), 401
+    current_user = decode_token_to_user(token)
+    if current_user and current_user in users:
+        return "Welcome {}".format(current_user), 200
+    else:
+        return jsonify({"message": "Invalid token"}), 401
 
 
-@app.route('/')
-@auth.login_required
-def index():
-    return "Hello, {}!".format(auth.current_user())
+def decode_token_to_user(token):
+    result = jwt.decode(token, app.config.get('SECRET_KEY'), algorithms="HS256")
+    return result['user'] if result else None
 
 
 if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
     app.run(host='0.0.0.0', port=5000, debug=True)
-
